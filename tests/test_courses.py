6917a1fbf7df9cc4a79005d71e5fbc4b24eb0dd4
davcost9nn <davcost@yandex.ru>
2025-09-24 14:14:02 +0300

    Image widget component

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,15 +1,45 @@
+from calendar import weekday
+
 import pytest
-from playwright.sync_api import expect, Page
+
+from pages.courses_list_page import CoursesListPage
+from pages.create_course_page import CreateCoursePage
 
 
-@pytest.mark.regression
 @pytest.mark.courses
-def test_empty_courses_list(chromium_page_with_state: Page):
-        chromium_page_with_state.goto("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
-        courses_title = chromium_page_with_state.get_by_test_id('courses-list-toolbar-title-text')
-        courses_results = chromium_page_with_state.get_by_test_id('courses-list-empty-view-title-text')
-        courses_description_results = chromium_page_with_state.get_by_test_id('courses-list-empty-view-description-text')
+@pytest.mark.regression
+def test_empty_courses_list(courses_list_page: CoursesListPage):
+    courses_list_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
+    courses_list_page.sidebar.check_visible()
+    courses_list_page.navbar.check_visible('username')
+    courses_list_page.toolbar_view.check_visible()
+    courses_list_page.check_visible_empty_view()
 
-        expect(courses_title).to_be_visible()
-        expect(courses_results).to_be_visible()
-        expect(courses_description_results).to_be_visible()
+
+def test_create_course(courses_list_page: CoursesListPage,create_course_page: CreateCoursePage):
+    create_course_page.visit('https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create')
+
+    create_course_page.check_visible_create_course_title()
+    create_course_page.check_disabled_create_course_button()
+    create_course_page.image_upload_widget.check_visible(is_image_uploaded=False)
+    create_course_page.check_visible_create_course_form(title='', max_score='0', min_score='0', description='', estimated_time='')
+
+    create_course_page.check_visible_exercises_title()
+    create_course_page.check_visible_create_exercise_button()
+    create_course_page.check_visible_exercises_empty_view()
+
+    create_course_page.image_upload_widget.upload_preview_image('./photo/Screenshot_3.png')
+    create_course_page.image_upload_widget.check_visible(is_image_uploaded=True)
+    create_course_page.fill_create_course_form(
+        title = 'Playwright',
+        max_score = '100',
+        min_score = '10',
+        description = 'Playwright',
+        estimated_time = '2 weeks'
+    )
+    create_course_page.click_create_course_button()
+
+    courses_list_page.toolbar_view.check_visible()
+    courses_list_page.course_view.check_visible(
+        index=0, title='Playwright', max_score= '100',min_score ='10', estimated_time = '2 weeks'
+    )
